aeaf285445403eaf43acbc04c43c7c837281639e
trungkien110298 <44560514+trungkien110298@users.noreply.github.com>
2018-12-08 16:32:51 +0700

    ddd

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
     'secret':'nodeauthsecret',
-    'database': 'mongodb://localhost:27017/MokiDB'
+    'database': 'mongodb://tuan98:tuan98bkhn@ds062339.mlab.com:62339/mokidb'
   };
